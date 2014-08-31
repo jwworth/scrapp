161d4d01e729f212a16afca59a266e1fa1912423
Jake Worth <jworth@prevailhs.com>
2014-08-31 14:44:53 -0500

    Add seed files

---
+++ db/fixtures/games.rb
@@ -0,0 +1,143 @@
+Game.seed do |g|
+  g.created_at = "2013-08-17"
+  g.winner_id = 2
+  g.winner_score = 274
+  g.loser_id = 1
+  g.loser_score = 271
+end
+
+Game.seed do |g|
+  g.created_at = "2013-08-20"
+  g.winner_id = 1
+  g.winner_score = 287
+  g.loser_id = 2
+  g.loser_score = 208
+end
+
+Game.seed do |g|
+  g.created_at = "2013-09-20"
+  g.winner_id = 1
+  g.winner_score = 311
+  g.loser_id = 2
+  g.loser_score = 239
+end
+
+Game.seed do |g|
+  g.created_at = "2013-09-27"
+  g.winner_id = 1
+  g.winner_score = 271
+  g.loser_id = 2
+  g.loser_score = 242
+end
+
+Game.seed do |g|
+  g.created_at = "2013-10-01"
+  g.winner_id = 2
+  g.winner_score = 278
+  g.loser_id = 1
+  g.loser_score = 244
+end
+
+Game.seed do |g|
+  g.created_at = "2013-10-15"
+  g.winner_id = 2
+  g.winner_score = 259
+  g.loser_id = 1
+  g.loser_score = 246
+end
+
+Game.seed do |g|
+  g.created_at = "2013-10-16"
+  g.winner_id = 2
+  g.winner_score = 283
+  g.loser_id = 1
+  g.loser_score = 279
+end
+
+Game.seed do |g|
+  g.created_at = "2013-10-28"
+  g.winner_id = 2
+  g.winner_score = 291
+  g.loser_id = 1
+  g.loser_score = 216
+end
+
+Game.seed do |g|
+  g.created_at = "2013-11-02"
+  g.winner_id = 2
+  g.winner_score = 317
+  g.loser_id = 1
+  g.loser_score = 254
+end
+
+Game.seed do |g|
+  g.created_at = "2014-02-01"
+  g.winner_id = 1
+  g.winner_score = 288
+  g.loser_id = 2
+  g.loser_score = 282
+end
+
+Game.seed do |g|
+  g.created_at = "2014-02-14"
+  g.winner_id = 1
+  g.winner_score = 296
+  g.loser_id = 2
+  g.loser_score = 280
+end
+
+Game.seed do |g|
+  g.created_at = "2014-03-06"
+  g.winner_id = 2
+  g.winner_score = 280
+  g.loser_id = 1
+  g.loser_score = 276
+end
+
+Game.seed do |g|
+  g.created_at = "2014-04-02"
+  g.winner_id = 2
+  g.winner_score = 280
+  g.loser_id = 1
+  g.loser_score = 213
+end
+
+Game.seed do |g|
+  g.created_at = "2014-05-06"
+  g.winner_id = 2
+  g.winner_score = 291
+  g.loser_id = 1
+  g.loser_score = 248
+end
+
+Game.seed do |g|
+  g.created_at = "2014-06-20"
+  g.winner_id = 1
+  g.winner_score = 254
+  g.loser_id = 2
+  g.loser_score = 247
+end
+
+Game.seed do |g|
+  g.created_at = "2014-06-21"
+  g.winner_id = 2
+  g.winner_score = 301
+  g.loser_id = 1
+  g.loser_score = 244
+end
+
+Game.seed do |g|
+  g.created_at = "2014-06-25"
+  g.winner_id = 1
+  g.winner_score = 336
+  g.loser_id = 1
+  g.loser_score = 329
+end
+
+Game.seed do |g|
+  g.created_at = "2014-08-04"
+  g.winner_id = 1
+  g.winner_score = 393
+  g.loser_id = 2
+  g.loser_score = 222
+end
---
+++ db/fixtures/players.rb
@@ -0,0 +1,9 @@
+Player.seed do |p|
+  p.id    = 1
+  p.name = "Jake"
+end
+
+Player.seed do |p|
+  p.id    = 2
+  p.name = "Steph"
+end
