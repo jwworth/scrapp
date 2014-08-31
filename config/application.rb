e9622a29d84644b3b003fb320d1ba2f0cd2ac3f2
Jake Worth <jworth@prevailhs.com>
2014-08-31 14:49:57 -0500

    Install Wwwhisper middleware

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,10 @@ module MyScrabble
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Insert wwwhisper as first middleware, if the addon is already installed
+    if ENV['WWWHISPER_URL']
+      config.middleware.insert 0, "Rack::WWWhisper"
+    end
   end
 end
