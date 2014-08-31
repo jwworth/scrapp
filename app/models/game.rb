4e7d8997db0e01816c82b2b37fef531fa03df1ad
Jake Worth <jworth@prevailhs.com>
2014-08-31 14:20:10 -0500

    Add Game model methods to get tests to pass

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,20 @@
 class Game < ActiveRecord::Base
+
+  ### ASSOCIATIONS
+  belongs_to :winner, class_name: 'Player'
+  belongs_to :loser,  class_name: 'Player'
+
+  ### VALIDATIONS
+  validates_presence_of :winner_id
+  validates_presence_of :winner_score
+  validates_presence_of :loser_id
+  validates_presence_of :loser_score
+
+  ### CALLBACKS
+  before_create :set_default_bingos
+
+  def set_default_bingos
+    self.bingos ||= 0
+  end
 end
+
