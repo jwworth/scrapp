44d43c462023d8cac4b9edd68e1da497b01ece49
Jake Worth <jworth@prevailhs.com>
2014-09-01 15:01:06 -0500

    Override Rails field_with_error wrappers

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Override Rails field_with_errors wrapper (https://coderwall.com/p/s-zwrg)
+ActionView::Base.field_error_proc = proc do |html_tag, instance|
+  html_tag.html_safe
+end
