a1e6c92d83c1139606c13de33036c143390acd98
Jake Worth <jworth@prevailhs.com>
2014-09-01 15:03:40 -0500

    Add underscore to indicate unused block arguments

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,6 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 # Override Rails field_with_errors wrapper (https://coderwall.com/p/s-zwrg)
-ActionView::Base.field_error_proc = proc do |html_tag, instance|
+ActionView::Base.field_error_proc = proc do |html_tag, _instance|
   html_tag.html_safe
 end
