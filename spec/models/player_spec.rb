1db305a9837fa8b60585dfa5217a711d113808e2
Jake Worth <jakeworth82@gmail.com>
2015-03-31 19:20:35 -0400

    Add a better model spec

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,11 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Player, type: :model do
+describe Player do
 
-  ### VALIDATIONS
-  it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:name) }
-  it { should allow_value('foobar').for(:name) }
-  it { should_not allow_value('a' * 16).for(:name) }
+  it 'should be have a valid factory' do
+    player = FactoryGirl.create(:player)
 
+    expect(player).to be_valid
+  end
+
+  it 'should validate uniquness of name' do
+    player = FactoryGirl.create(:player, name: 'foobar')
+    dup_player = FactoryGirl.create(:player, name: 'foobar')
+
+    expect(dup_player).to_not be_valid
+  end
+
+  it 'should validate length of name' do
+    player = FactoryGirl.build(:player, name: 'a' * 16)
+
+    expect(player).to_not be_valid
+  end
+
+  it 'should standardize caplitalization' do
+    player = FactoryGirl.create(:player, name: 'foOBar')
+
+    expect(player.name).to eq('Foobar')
+  end
 end
