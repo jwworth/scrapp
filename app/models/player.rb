9a141554f694deed41b2302f0321d46543f98bd1
Jake Worth <jakeworth82@gmail.com>
2015-03-31 19:18:56 -0400

    Remove redundant comments

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,10 +1,9 @@
 class Player < ActiveRecord::Base
-  ### VALIDATIONS
+
   validates_presence_of :name
   validates_length_of :name, maximum: 15
   validates_uniqueness_of :name
 
-  ### CALLBACKS
   before_save :standardize_capitalization
 
   def standardize_capitalization
