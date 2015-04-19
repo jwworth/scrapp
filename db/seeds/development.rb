da0eebee2240012157b89e9d7045a03316fab02b
Jake Worth <jakeworth82@gmail.com>
2015-04-19 17:58:33 -0500

    Further randomize the seeds

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,8 +1,10 @@
-winner = Player.create!(name: 'jack')
-loser  = Player.create!(name: 'jill')
+jack = Player.create!(name: 'jack')
+jill = Player.create!(name: 'jill')
 
 10.times do |i|
+  rand(2) == 1 ? winner, loser = jack, jill : winner, loser = jill, jack
   winner_score, loser_score = rand(250..300), rand(150..220)
+
   puts "Creating game ##{i}"
-  Game.create!(winner: winner, winner_score: winner_score, loser: loser, loser_score: loser_score)
+  Game.create!(winner: winner, winner_score: winner_score, loser: loser, loser_score: loser_score, bingos: rand(2))
 end
