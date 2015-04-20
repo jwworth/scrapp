b82744e78212d713bdde2f0ba58777dcee60cf00
Jake Worth <jakeworth82@gmail.com>
2015-04-20 18:03:47 -0500

    Give user more information on db:seed

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,10 +1,16 @@
-jack = Player.create!(name: 'jack')
-jill = Player.create!(name: 'jill')
+jack = Player.find_or_create_by!(name: 'jack')
+jill = Player.find_or_create_by!(name: 'jill')
+puts "Found or created players Jack and Jill"
 
-10.times do |i|
-  rand(2) == 1 ? winner, loser = jack, jill : winner, loser = jill, jack
+10.times do
+  if rand(2) == 1
+    winner, loser = jack, jill
+  else
+    winner, loser = jill, jack
+  end
+
   winner_score, loser_score = rand(250..300), rand(150..220)
 
-  puts "Creating game ##{i}"
-  Game.create!(winner: winner, winner_score: winner_score, loser: loser, loser_score: loser_score, bingos: rand(2))
+  game = Game.create!(winner: winner, winner_score: winner_score, loser: loser, loser_score: loser_score, bingos: rand(2))
+  puts "Created game ##{game.id}"
 end
