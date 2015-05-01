ea5bfb7824df4e534503c980537d3bf7fe4479cd
Jake Worth <jakeworth82@gmail.com>
2015-05-01 15:05:10 -0500

    Add http authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  if ENV['http_auth']
+    http_basic_authenticate_with name: ENV['http_name'], password: ENV['http_password']
+  end
 end
