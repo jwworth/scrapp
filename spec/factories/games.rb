e4e2f2c1b5211be94e0e611e0705f9b05f51b09c
Jake Worth <jakeworth82@gmail.com>
2015-05-19 22:03:57 -0500

    Allow game factory to create all by itself

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,4 +1,8 @@
 FactoryGirl.define do
   factory :game do
+    winner { FactoryGirl.create(:player, name: 'Winner') }
+    loser { FactoryGirl.create(:player, name: 'Loser') }
+    winner_score 100
+    loser_score 80
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,55 +1,44 @@
 require 'rails_helper'
 
 describe Game do
-  let(:winner) { FactoryGirl.create(:player) }
-  let(:loser) { FactoryGirl.create(:player) }
-
   it 'should have a valid factory' do
-    game = FactoryGirl.create(:game, winner: winner, winner_score: 200, loser: loser, loser_score: 100)
-
+    game = FactoryGirl.create(:game)
     expect(game).to be_valid
   end
 
   it 'should require a winner' do
-    game = FactoryGirl.build(:game, winner_score: 200, loser: loser, loser_score: 100)
-
+    game = FactoryGirl.build(:game, winner: nil)
     expect(game).to_not be_valid
   end
 
   it 'should require a loser' do
-    game = FactoryGirl.build(:game, winner_score: 200, winner: winner, loser_score: 100)
-
+    game = FactoryGirl.build(:game, loser: nil)
     expect(game).to_not be_valid
   end
 
   it 'should validate numericality of winner score' do
-    game = FactoryGirl.build(:game, winner: winner, winner_score: 'foo', loser: loser, loser_score: 100)
-
+    game = FactoryGirl.build(:game, winner_score: 'foo')
     expect(game).to_not be_valid
   end
 
   it 'should validate numericality of loser score' do
-    game = FactoryGirl.build(:game, winner: winner, winner_score: 100, loser: loser, loser_score: 'bar')
-
+    game = FactoryGirl.build(:game, loser_score: 'bar')
     expect(game).to_not be_valid
   end
 
   it 'should provider player names' do
-    game = FactoryGirl.create(:game, winner: winner, winner_score: 200, loser: loser, loser_score: 100)
-
-    expect(game.winner_name).to eq winner.name
-    expect(game.loser_name).to eq loser.name
+    game = FactoryGirl.create(:game)
+    expect(game.winner_name).to eq 'Winner'
+    expect(game.loser_name).to eq 'Loser'
   end
 
   it 'should set default bingos on create' do
-    game = FactoryGirl.create(:game, winner: winner, winner_score: 100, loser: loser, loser_score: 90)
-
+    game = FactoryGirl.create(:game)
     expect(game.bingos).to eq 0
   end
 
   it 'should validate that the winner score is greater than the loser score' do
-    game = FactoryGirl.build(:game, winner: winner, winner_score: 100, loser: loser, loser_score: 101)
-
+    game = FactoryGirl.build(:game, winner_score: 100, loser_score: 101)
     expect(game).to_not be_valid
   end
 end
