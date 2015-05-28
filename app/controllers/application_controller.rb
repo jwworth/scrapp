7ca0a472688b5534fd568ed7a532763f4c6d159d
Jake Worth <jakeworth82@gmail.com>
2015-05-27 22:26:44 -0500

    One variable to rule all the authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  if ENV['http_auth']
-    http_basic_authenticate_with name: ENV['http_name'], password: ENV['http_password']
+  if creds = ENV['basic_auth']
+    username, password = creds.split(':', 2)
+    http_basic_authenticate_with name: username, password: password
   end
 end
